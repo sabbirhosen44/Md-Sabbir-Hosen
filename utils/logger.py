35f9d68aa898689c7350324ed7ae8f3436473fb3
sabbir <sabbirhosen.pstu@gmail.com>
2026-06-15 17:32:35 +0600

    Add file-based logging and update gitignore

--- utils/logger.py
+++ utils/logger.py
@@ -1,9 +1,13 @@
 import logging
+import os
 
+os.makedirs("logs", exist_ok=True)
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s",
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    filename="logs/app.log",
+    filemode="a",
 )
 
 logger = logging.getLogger(__name__)
